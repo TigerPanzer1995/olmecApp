1629d6275a7906771560c2fd8951eade54638dfa
TigerPanzer1995 <cellsoherrera@gmail.com>
2020-11-12 08:05:14 -0600

    Versión 1.0.0.2 Version a revisión 12/11/2020

--- app/src/main/java/com/example/olmecapp/Laguna.java
+++ app/src/main/java/com/example/olmecapp/Laguna.java
@@ -3,17 +3,23 @@ package com.example.olmecapp;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.drawerlayout.widget.DrawerLayout;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 
-public class Laguna extends AppCompatActivity {
+public class Laguna extends AppCompatActivity implements View.OnClickListener {
     DrawerLayout drawerLayout;
+    private static final String apompo = "https://www.uv.mx/noticias/2015/06/11/pachira-aquatica-o-apompo-solucion-forestal-para-humedales-tropicales/#:~:text=Pachira%20aquatica%2C%20explic%C3%B3%2C%20es%20un,los%20cauces%20de%20los%20r%C3%ADos.";
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)  {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_laguna);
         drawerLayout = findViewById(R.id.drawer_layout);
+        Button btn_apom = findViewById(R.id.btn_apompo);
+        btn_apom.setOnClickListener(this);
 
     }//fin del oncreate
     public void ClickMenu(View view){
@@ -48,5 +54,18 @@ public class Laguna extends AppCompatActivity {
     protected void onPause() {
         super.onPause();
         Inicio.closeDrawer(drawerLayout);
+    }
+
+    @Override
+    public void onClick(View view) {
+        Intent intent = new Intent(Intent.ACTION_VIEW);
+        switch (view.getId()){
+            case R.id.btn_apompo:
+                intent.setData(Uri.parse(apompo));
+                startActivity(intent);
+
+                break;
+        }
+
     }
 }//fin de la clase
